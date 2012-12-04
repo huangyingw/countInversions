5b8af98661aa3bb4a3620fff12faca31a9b46eed
huangyingw@linux_programming <huangyingw@gmail.com>
2012-12-03 23:15:49 -0800

    n

--- src/CountInversions.java
+++ src/CountInversions.java
@@ -1,30 +1,30 @@
 public class CountInversions {
 
 	static int mergeSort(int arr[]) {
-		int[] temp = new int[arr.length];
-		return _mergeSort(arr, temp, 0, arr.length - 1);
+		return _mergeSort(arr, 0, arr.length - 1);
 	}
 
-	static int _mergeSort(int arr[], int temp[], int left, int right) {
+	static int _mergeSort(int arr[], int left, int right) {
 		int mid, inv_count = 0;
 		if (right > left) {
 			mid = (right + left) / 2;
 
-			inv_count = _mergeSort(arr, temp, left, mid);
-			inv_count += _mergeSort(arr, temp, mid + 1, right);
+			inv_count = _mergeSort(arr, left, mid);
+			inv_count += _mergeSort(arr, mid + 1, right);
 
-			inv_count += merge(arr, temp, left, mid + 1, right);
+			inv_count += merge(arr, left, mid + 1, right);
 		}
 		return inv_count;
 	}
 
-	static int merge(int arr[], int result[], int left, int mid, int right) {
+	static int merge(int arr[], int left, int mid, int right) {
 		int lIndex, rIndex, resultIndex;
 		int inv_count = 0;
 
 		lIndex = left;
 		rIndex = mid;
 		resultIndex = left;
+		int[] result = new int[arr.length];
 		while ((lIndex <= mid - 1) && (rIndex <= right)) {
 			if (arr[lIndex] <= arr[rIndex]) {
 				result[resultIndex++] = arr[lIndex++];
